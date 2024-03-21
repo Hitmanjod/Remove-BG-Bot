f2f2380c4bdfd018e6c3b2f1eee97f3ccc12f9ee
Hitmanjod <142624499+Hitmanjod@users.noreply.github.com>
2024-03-21 10:25:01 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,5 +7,5 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
-REMOVEBG_API = os.environ.get("REMOVEBG_API", "")
+REMOVEBG_API = os.environ.get("REMOVEBG_API", "2M9M72cYLU6ooyRhDiZdfEuB")
 UNSCREEN_API = os.environ.get("UNSCREEN_API", "")
